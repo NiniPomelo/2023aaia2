ca9cc4c99854dc22549c7b751e4f5e32c569d339
NiniPomelo <n0966785408@gmail.com>
2024-03-20 10:28:52 +0800

    week05

---
+++ week05/week05-2a.cpp
@@ -0,0 +1,11 @@
+///從 week05-1.cpp 複製過來 week05-2a.cpp
+///CodeBlocks右鍵跳出視窗 , Format use Astyle
+
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++)
+    {
+        printf("i是%d", i);
+    }
+}
---
+++ week05/week05-2c.cpp
@@ -0,0 +1,13 @@
+///從 week05-1.cpp 複製過來 week05-2a.cpp
+///Settings-Editor (左邊第七個)Source Formatter
+///選 Stroustrup(C++的爸爸)
+///CodeBlocks右鍵跳出視窗 , Format use Astyle
+
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++) {
+        printf("i是%d ", i);
+    }
+}
+
