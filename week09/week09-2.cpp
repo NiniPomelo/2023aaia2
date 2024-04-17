680cb268cddfb633fa72333a5a7737ac445684a5
NiniPomelo <n0966785408@gmail.com>
2024-04-17 10:27:04 +0800

    week09

---
+++ week09/week09-2.cpp
@@ -0,0 +1,15 @@
+///week09-2.cpp 要了解字串與陣列關係
+#include <stdio.h>
+int main()
+{
+
+    char line[80]; ///用一行字讀進來
+    printf("請輸入一行英文，有空格沒關係:\n");
+    scanf("%s", line); ///課本 6-16頁
+    printf("這行字是:%s\n",line);
+    printf("請再輸入一行英文:\n");
+    scanf("%s",line); ///課本 6-16頁，空隔斷開
+    printf("這行字是:%\n", line);
+
+
+}
