11e159ec5a9a03934d487293c5baa843e1f08f01
NiniPomelo <n0966785408@gmail.com>
2024-05-01 12:00:56 +0800

    week11

---
+++ week11/week11-5a.py
@@ -0,0 +1,14 @@
+//part 1
+#include <stdio.h>
+int main()
+{
+	int a[30] [30];
+	int m, n;
+	scanf("%d %d", &m, &n);
+	for(int i=0; i<m; i++){ //part 1
+		for(int j=0; j<n; j++){
+			scanf("%d", &a[i][j] );
+		}
+	}
+
+}
