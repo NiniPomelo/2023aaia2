5308e203137dc87fc171f03a6ac9238909f04052
NiniPomelo <n0966785408@gmail.com>
2024-05-08 11:47:00 +0800

    week12

--- week12/week12-3.cpp
+++ week12/week12-3.cpp
@@ -1,5 +1,5 @@
 /// week12-2.cpp 列出 n以下的全部質數
-# include <stdio.h>
+#include <stdio.h>
 int isPrime(int n)
 { ///質數不能整除
     for(int i=2; i<n; i++){
@@ -12,7 +12,13 @@ int main()
     printf("請輸入1個數:");
     int n;
     scanf("%d",&n);
+    int ans =0;
     for(int i=2; i<=n; i++){
-        if(isPrime(i)) printf("%d ",i);
+        if(isPrime(i)){
+            printf("%d ",i);
+            ans ++;
+        }
     }
+    printf("總共有 %d 個質數\n", ans);
 }
+
