264e4913ac27b37340a87863d6b2e94c37c72307
unknown <n0966785408@gmail.com>
2024-05-29 11:02:15 +0800

    week15

---
+++ week15/week15-3.c
@@ -0,0 +1,14 @@
+// week15-3.c
+int equalSubstring(char* s, char* t, int maxCost) { //week15-1.c
+    int N = strlen(s); //先知道字串長度
+    int ans = 0, j = 0;
+    for(int i=0; i<N; i++){ // 字串的迴圈
+        maxCost -= abs( s[i] - t[i] ); // C語言直接減 i頭
+        while(maxCost<0){
+            maxCost += abs( s[j] - t[j] ); //小心，是j尾巴
+            j += 1;
+        }
+        if(i-j+1>ans) ans = i-j+1; //更新答案
+    }
+    return ans; 
+}
